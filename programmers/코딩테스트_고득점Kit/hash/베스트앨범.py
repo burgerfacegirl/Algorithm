82996245989c13659a0df3de43449a777883e0f5
burgerfacegirl <109276569+burgerfacegirl@users.noreply.github.com>
2022-12-24 00:23:43 +0900

    221223

--- programmers/코딩테스트_고득점Kit/hash/베스트앨범.py
+++ programmers/코딩테스트_고득점Kit/hash/베스트앨범.py
@@ -10,13 +10,27 @@ def solution(genres, plays):
     for i in range(len(genres)):
         dict[genres[i]] += plays[i]
         dict_cnt[genres[i]].append([plays[i], i])
-    print(dict_cnt)
-    for item in dict_cnt.items():
-        print(item)
+
     dict = sorted(dict.items(), key = lambda item : item[0], reverse = True)
-    dict_cnt = sorted(dict_cnt.items(), key = lambda  x: (-x[0], x[1]))
 
     answer = []
-    return dict, dict_cnt
+
+    ### lambda를 활용한 list, dict 정렬 더 공부해야겠다, 정렬 기준인 key가 뭐가 되는지 정렬된 결과가 어떻게 나오는지 등
+    for genre in dict:
+        ranked_song = sorted(dict_cnt[genre[0]], key=lambda  x: (-x[0], x[1]))
+        best_album = 0
+        for song in ranked_song:
+            answer.append(song[1])
+            best_album += 1
+            if best_album == 2:
+                break
+        if len(answer) == 4:
+            break
+    # dict_cnt = sorted(dict_cnt.values(), key = lambda x: (-x[0], x[1]))
+        # genres_set[genre] = sorted(dict_cnt.values(), key = lambda  x: (-x[0], x[1]))
+    # dict_cnt = sorted(dict_cnt.items(), key = lambda  x: (-x[0], x[1]))
+
+    return answer
 
 print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
+
