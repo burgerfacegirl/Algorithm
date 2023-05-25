f9fa5f02ecacb92bcbd76f225779bd7e5e21cf2d
hojung <hoguangel@gmail.com>
2023-05-25 10:13:46 +0900

    230525_boj_1816_암호키

---
+++ 애옹스터디/복습/1816_암호키.py
@@ -0,0 +1,14 @@
+# boj_1816_암호키
+
+N = int(input())
+
+for i in range(N):
+    ans = 'YES'
+    num = int(input())
+
+    for j in range(2, 10**6+1):
+        if num % j == 0:
+            ans = 'NO'
+            break
+    print(ans)
+
