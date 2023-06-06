5d631e66c79df44624a1bd032d654013df100658
hojung <hoguangel@gmail.com>
2023-06-06 18:57:08 +0900

    [programmers]올바른 괄호_level2

--- programmers/코딩테스트연습/level_2/올바른괄호.py
+++ programmers/코딩테스트연습/level_2/올바른괄호.py
@@ -1,36 +1,30 @@
 def solution(s):
     answer = True
 
-    s = list(s)
-    close = 0
-    open = 0
-    cnt = 0
-    before = False
+    closed = []
 
-    if s[-1] == "(" or s[0] == ")":
+    if s[0] == ")" or s[-1] == "(":
         answer = False
     else:
         while s:
-            #             다른 괄호가 2번 나오면 그때까지 쌓인 close/open 개수 비교
             letter = s.pop()
-            if before:
-                if before != letter:
-                    cnt += 1
-            elif before == False and s[-1] != s[-2]:
-                cnt += 1
 
             if letter == ")":
-                close += 1
-            elif letter == "(":
-                open += 1
-            before = letter
-            if cnt == 2:
-                if close != open:
+                closed.append(letter)
+            else:
+                if closed:
+                    closed.pop()
+                    s.pop()
+                else:
                     answer = False
                     break
-                else:
-                    cnt = 0
-                    close = 0
-                    open = 0
+
+            if answer == False:
+                break
+
+        if closed:
+            answer = False
 
     return answer
+
+
