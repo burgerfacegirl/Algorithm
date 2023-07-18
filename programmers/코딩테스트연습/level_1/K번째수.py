34e795dcf3f1ea0ae63b584618819b493899a53f
burgerfacegirl <109276569+burgerfacegirl@users.noreply.github.com>
2023-07-18 21:20:35 +0900

    [programmers]K번째수_level1

---
+++ programmers/코딩테스트연습/level_1/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        i, j, k = command[0], command[1], command[2]
+        sliced = array[i - 1:j]
+        sliced.sort()
+        answer.append(sliced[k - 1])
+
+    return answer
